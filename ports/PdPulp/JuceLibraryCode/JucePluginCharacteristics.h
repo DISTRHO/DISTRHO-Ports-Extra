9f10632fe69d5066e597f2ec064715306a0858e2
falkTX <falktx@gmail.com>
2015-08-20 12:32:54 +0200

    Import PdPulp code

---
+++ ports/PdPulp/JuceLibraryCode/JucePluginCharacteristics.h
@@ -0,0 +1,135 @@
+/*
+
+    IMPORTANT! This file is auto-generated each time you save your
+    project - if you alter its contents, your changes may be overwritten!
+
+    There's a section below where you can add your own custom code safely, and the
+    Introjucer will preserve the contents of that block, but the best way to change
+    any of these definitions is by using the Introjucer's project settings.
+
+    Any commented-out settings will assume their default values.
+
+*/
+
+#ifndef __JUCE_APPCONFIG_LIBPDPL__
+#define __JUCE_APPCONFIG_LIBPDPL__
+
+//==============================================================================
+// Audio plugin settings..
+
+#if PULP_SYNTH
+ #define JucePlugin_Name                   "Pd Pulp"
+ #define JucePlugin_MaxNumInputChannels    2
+ #define JucePlugin_MaxNumOutputChannels   2
+ #define JucePlugin_PreferredChannelConfigurations  {0, 1}, {0, 2}
+ #define JucePlugin_IsSynth                1
+ #define JucePlugin_WantsMidiInput         1
+ #define JucePlugin_VSTCategory            kPlugCategSynth
+ #define JucePlugin_PluginCode             'PDLP'
+#else
+ #define JucePlugin_Name                   "Pd Pulp FX"
+ #define JucePlugin_MaxNumInputChannels    2
+ #define JucePlugin_MaxNumOutputChannels   2
+ #define JucePlugin_PreferredChannelConfigurations  {1, 1}, {2, 2}
+ #define JucePlugin_IsSynth                0
+ #define JucePlugin_WantsMidiInput         0
+ #define JucePlugin_VSTCategory            kPlugCategEffect
+ #define JucePlugin_PluginCode             'PDLF'
+#endif
+
+#ifndef  JucePlugin_Desc
+ #define JucePlugin_Desc                   "a pure data audio plugin runtime environment"
+#endif
+#ifndef  JucePlugin_Manufacturer
+ #define JucePlugin_Manufacturer           "Audiosalt"
+#endif
+#ifndef  JucePlugin_ManufacturerWebsite
+ #define JucePlugin_ManufacturerWebsite    "audiosalt.com"
+#endif
+#ifndef  JucePlugin_ManufacturerEmail
+ #define JucePlugin_ManufacturerEmail      ""
+#endif
+#ifndef  JucePlugin_ManufacturerCode
+ #define JucePlugin_ManufacturerCode       'ASLT'
+#endif
+#ifndef  JucePlugin_ProducesMidiOutput
+ #define JucePlugin_ProducesMidiOutput     0
+#endif
+#ifndef  JucePlugin_SilenceInProducesSilenceOut
+ #define JucePlugin_SilenceInProducesSilenceOut  0
+#endif
+#ifndef  JucePlugin_EditorRequiresKeyboardFocus
+ #define JucePlugin_EditorRequiresKeyboardFocus  1
+#endif
+#ifndef  JucePlugin_Version
+ #define JucePlugin_Version                1.0.0
+#endif
+#ifndef  JucePlugin_VersionCode
+ #define JucePlugin_VersionCode            0x10000
+#endif
+#ifndef  JucePlugin_VersionString
+ #define JucePlugin_VersionString          "1.0.0"
+#endif
+#ifndef  JucePlugin_VSTUniqueID
+ #define JucePlugin_VSTUniqueID            JucePlugin_PluginCode
+#endif
+#ifndef  JucePlugin_AUMainType
+ #define JucePlugin_AUMainType             kAudioUnitType_MusicDevice
+#endif
+#ifndef  JucePlugin_AUSubType
+ #define JucePlugin_AUSubType              JucePlugin_PluginCode
+#endif
+#ifndef  JucePlugin_AUExportPrefix
+ #define JucePlugin_AUExportPrefix         PdPulpAU
+#endif
+#ifndef  JucePlugin_AUExportPrefixQuoted
+ #define JucePlugin_AUExportPrefixQuoted   "PdPulpAU"
+#endif
+#ifndef  JucePlugin_AUManufacturerCode
+ #define JucePlugin_AUManufacturerCode     JucePlugin_ManufacturerCode
+#endif
+#ifndef  JucePlugin_CFBundleIdentifier
+ #define JucePlugin_CFBundleIdentifier     com.audiosalt.pdpulp
+#endif
+#ifndef  JucePlugin_RTASCategory
+ #define JucePlugin_RTASCategory           ePlugInCategory_SWGenerators
+#endif
+#ifndef  JucePlugin_RTASManufacturerCode
+ #define JucePlugin_RTASManufacturerCode   JucePlugin_ManufacturerCode
+#endif
+#ifndef  JucePlugin_RTASProductId
+ #define JucePlugin_RTASProductId          JucePlugin_PluginCode
+#endif
+#ifndef  JucePlugin_RTASDisableBypass
+ #define JucePlugin_RTASDisableBypass      0
+#endif
+#ifndef  JucePlugin_RTASDisableMultiMono
+ #define JucePlugin_RTASDisableMultiMono   0
+#endif
+#ifndef  JucePlugin_AAXIdentifier
+ #define JucePlugin_AAXIdentifier          com.audiosalt.pdpulp
+#endif
+#ifndef  JucePlugin_AAXManufacturerCode
+ #define JucePlugin_AAXManufacturerCode    JucePlugin_ManufacturerCode
+#endif
+#ifndef  JucePlugin_AAXProductId
+ #define JucePlugin_AAXProductId           JucePlugin_PluginCode
+#endif
+#ifndef  JucePlugin_AAXCategory
+ #define JucePlugin_AAXCategory            AAX_ePlugInCategory_Dynamics
+#endif
+#ifndef  JucePlugin_AAXDisableBypass
+ #define JucePlugin_AAXDisableBypass       0
+#endif
+#ifndef  JucePlugin_AAXDisableMultiMono
+ #define JucePlugin_AAXDisableMultiMono    0
+#endif
+
+#define JucePlugin_LV2URI "http://pdpulp.audiosalt.com"
+
+#define JucePlugin_WantsLV2Presets 0
+#define JucePlugin_WantsLV2State   1
+#define JucePlugin_WantsLV2TimePos 0
+#define JucePlugin_WantsLV2FixedBlockSize 1
+
+#endif  // __JUCE_APPCONFIG_LIBPDPL__
---
+++ ports/PdPulp/LV2-ins/premake.lua
@@ -0,0 +1,109 @@
+
+dofile("../../../scripts/make-project.lua")
+
+package = make_juce_lv2_project("PdPulp")
+
+package.buildoptions = {
+    package.buildoptions,
+    "-Wno-unused",
+    "-Wno-unused-parameter",
+    "-Wno-parentheses",
+    "-Wno-switch"
+}
+
+package.defines = {
+  package.defines,
+  "PULP_SYNTH=1",
+  "USEAPI_DUMMY",
+  "PD",
+  "HAVE_LIBDL",
+  "HAVE_UNISTD_H",
+  "HAVE_ALLOCA_H",
+  "_LARGEFILE64_SOURCE"
+}
+
+package.includepaths = {
+  package.includepaths,
+  "../Source",
+  "../JuceLibraryCode",
+  "../libpd/libpd_wrapper",
+  "../libpd/pure-data/src"
+}
+
+package.files = {
+  matchfiles (
+    "../Source/*.c",
+    "../Source/*.cpp",
+    "../libpd/pure-data/src/g_canvas.c",
+    "../libpd/pure-data/src/g_graph.c",
+    "../libpd/pure-data/src/g_text.c",
+    "../libpd/pure-data/src/g_rtext.c",
+    "../libpd/pure-data/src/g_array.c",
+    "../libpd/pure-data/src/g_template.c",
+    "../libpd/pure-data/src/g_io.c",
+    "../libpd/pure-data/src/g_scalar.c",
+    "../libpd/pure-data/src/g_traversal.c",
+    "../libpd/pure-data/src/g_guiconnect.c",
+    "../libpd/pure-data/src/g_readwrite.c",
+    "../libpd/pure-data/src/g_editor.c",
+    "../libpd/pure-data/src/g_all_guis.c",
+    "../libpd/pure-data/src/g_bang.c",
+    "../libpd/pure-data/src/g_hdial.c",
+    "../libpd/pure-data/src/g_hslider.c",
+    "../libpd/pure-data/src/g_mycanvas.c",
+    "../libpd/pure-data/src/g_numbox.c",
+    "../libpd/pure-data/src/g_toggle.c",
+    "../libpd/pure-data/src/g_vdial.c",
+    "../libpd/pure-data/src/g_vslider.c",
+    "../libpd/pure-data/src/g_vumeter.c",
+    "../libpd/pure-data/src/m_pd.c",
+    "../libpd/pure-data/src/m_class.c",
+    "../libpd/pure-data/src/m_obj.c",
+    "../libpd/pure-data/src/m_atom.c",
+    "../libpd/pure-data/src/m_memory.c",
+    "../libpd/pure-data/src/m_binbuf.c",
+    "../libpd/pure-data/src/m_conf.c",
+    "../libpd/pure-data/src/m_glob.c",
+    "../libpd/pure-data/src/m_sched.c",
+    "../libpd/pure-data/src/s_main.c",
+    "../libpd/pure-data/src/s_inter.c",
+    "../libpd/pure-data/src/s_file.c",
+    "../libpd/pure-data/src/s_print.c",
+    "../libpd/pure-data/src/s_loader.c",
+    "../libpd/pure-data/src/s_path.c",
+    "../libpd/pure-data/src/s_audio.c",
+    "../libpd/pure-data/src/s_audio_dummy.c",
+    "../libpd/pure-data/src/s_utf8.c",
+    "../libpd/pure-data/src/s_audio_paring.c",
+    "../libpd/pure-data/src/d_ugen.c",
+    "../libpd/pure-data/src/d_ctl.c",
+    "../libpd/pure-data/src/d_arithmetic.c",
+    "../libpd/pure-data/src/d_osc.c",
+    "../libpd/pure-data/src/d_filter.c",
+    "../libpd/pure-data/src/d_dac.c",
+    "../libpd/pure-data/src/d_misc.c",
+    "../libpd/pure-data/src/d_math.c",
+    "../libpd/pure-data/src/d_fft.c",
+    "../libpd/pure-data/src/d_fft_fftsg.c",
+    "../libpd/pure-data/src/d_array.c",
+    "../libpd/pure-data/src/d_global.c",
+    "../libpd/pure-data/src/d_delay.c",
+    "../libpd/pure-data/src/d_resample.c",
+    "../libpd/pure-data/src/d_soundfile.c",
+    "../libpd/pure-data/src/x_arithmetic.c",
+    "../libpd/pure-data/src/x_connective.c",
+    "../libpd/pure-data/src/x_interface.c",
+    "../libpd/pure-data/src/x_midi.c",
+    "../libpd/pure-data/src/x_misc.c",
+    "../libpd/pure-data/src/x_time.c",
+    "../libpd/pure-data/src/x_acoustics.c",
+    "../libpd/pure-data/src/x_net.c",
+    "../libpd/pure-data/src/x_text.c",
+    "../libpd/pure-data/src/x_gui.c",
+    "../libpd/pure-data/src/x_list.c",
+    "../libpd/pure-data/src/x_array.c",
+    "../libpd/pure-data/src/x_scalar.c",
+    "../libpd/libpd_wrapper/*.c",
+    "../../../libs/juce-plugin/JucePluginMain.cpp"
+  )
+}
